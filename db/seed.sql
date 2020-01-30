c369c5625160d3b3f33bb97aff6a242ccedac71e
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-01-29 22:19:13 -0700

    fixing the strings in the seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO 
     department (name) 
 VALUES
@@ -8,3 +6,19 @@ VALUES
     ("Engineering"),
     ("Finance");
 
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 70000, 3),
+       ("Engineer", 90000, 12),
+       ("Lawyer", 90000, 24,),
+       ("Acountant", 70000, 13),
+       ("Sales Person", 70000, 35);
+
+INSERT INTO employee (first_name, last_name, role, manager_id)
+VALUES ("Sarah", "Smith", "Sales Lead", 444),
+       ("Tom", "Hamm", "Engineer", NULL),
+       ("Kevin", "Blue", "Lawyer", NULL),
+       ("Susan", "Brown", "Accountant", NULL),
+       ("Bill", "Boyle", "Sales Person", NULL);
+
+
+    
