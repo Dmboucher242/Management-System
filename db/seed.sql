8c9b6918680eadbfda30344fda872160e82e9c35
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-26 20:12:54 -0700

    adding a git ignore file

--- db/seed.sql
+++ db/seed.sql
@@ -1,24 +1,33 @@
-INSERT INTO 
-    department (name) 
+use employees;
+
+INSERT INTO department
+    (name)
 VALUES
-    ("Legal"),
-    ("Sales"),
-    ("Engineering"),
-    ("Finance");
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Sales Lead", 70000, 3),
-       ("Engineer", 90000, 12),
-       ("Lawyer", 90000, 24,),
-       ("Acountant", 70000, 13),
-       ("Sales Person", 70000, 35);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 10000, 1),
+    ('Salesperson', 8000, 1),
+    ('Lead Engineer', 10000, 2),
+    ('Software Engineer', 10000, 2),
+    ('Account Manager', 16000, 3),
+    ('Accountant', 15000, 3),
+    ('Legal Team Lead', 20000, 4),
+    ('Lawyer', 10000, 4);
 
-INSERT INTO employee (first_name, last_name, role, manager_id)
-VALUES ("Sarah", "Smith", "Sales Lead", 444),
-       ("Tom", "Hamm", "Engineer", NULL),
-       ("Kevin", "Blue", "Lawyer", NULL),
-       ("Susan", "Brown", "Accountant", NULL),
-       ("Bill", "Boyle", "Sales Person", NULL);
-
-
-    
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('George', 'Norris', 1, NULL),
+    ('Gerald', 'Potter', 2, 1),
+    ('Ashley', 'Smith', 3, NULL),
+    ('Kevin', 'Mills', 4, 3),
+    ('Kim', 'Jones', 5, NULL),
+    ('Makayla', 'Brown', 6, 5),
+    ('Sarah', 'Gill', 7, NULL),
+    ('Tom', 'Sawyer', 8, 7);
